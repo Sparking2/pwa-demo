3445e8a7b96b736931162bc34b1e3b430caf65ea
Sparking <sparking02@yahoo.com.mx>
2023-06-22 14:12:10 -0600

    ✨ feat(pwa): add vite-plugin-pwa to devDependencies and configure it in vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+/* @type {import('vite').UserConfig} */
+
+import {VitePWA} from 'vite-plugin-pwa'
+import {defineConfig} from "vite";
+
+export default defineConfig({
+    plugins: [
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true
+            }
+        })
+    ]
+})
