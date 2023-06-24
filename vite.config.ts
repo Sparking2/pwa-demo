30c764d32c653cdc8ecb4f23aca5b082d073d015
Sparking <sparking02@yahoo.com.mx>
2023-06-23 18:00:38 -0600

    ✨ feat(pwa): add PWA assets and configuration
    Add PWA assets and configuration to the project, including icons, manifest file, and service worker configuration. Also, add a robots.txt file to allow all user agents to access the website.

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,41 @@ import {defineConfig} from "vite";
 export default defineConfig({
     plugins: [
         VitePWA({
+            // injectRegister: "script",
             registerType: 'autoUpdate',
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,webp}']
+            },
+            manifest: {
+                name: "PWA - DEMO",
+                short_name: "PWA",
+                description: "simple app to test how to make a PWA app with vite",
+                theme_color: "#ffffff",
+                icons: [
+                    {
+                        src: 'pwa-64x64.png',
+                        sizes: '64x64',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any'
+                    },
+                    {
+                        src: 'maskable-icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'maskable'
+                    }
+                ]
+            },
             devOptions: {
                 enabled: true
             }
